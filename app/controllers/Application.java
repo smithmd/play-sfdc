b25c9386182c08ec7c2c154d9e9972cadb64d67c
Michael Smith <smithmd@interlochen.org>
2015-03-11 16:51:45 -0400

    testing vars

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,8 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        String test = System.getenv().get("test");
+        return ok(index.render(test));
     }
 
 }
