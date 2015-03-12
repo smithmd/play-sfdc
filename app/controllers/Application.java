9677d6d53ad7aa44f720be203dce52dd2888aef8
Michael Smith <smithmd@interlochen.org>
2015-03-12 15:55:39 -0400

    dropping the extra 3 characters

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -38,7 +38,7 @@ public class Application extends Controller {
         String output = "";
         String accessToken = null;
 
-        String dashboardId = "01ZJ00000000MNBMA2";
+        String dashboardId = "01ZJ00000000MNB";
         ENVIRONMENT = (System.getenv().get("IS_LIVE").equals("1") ? "_LIVE" : "_TEST");
 
         String token = requestAccessToken();
