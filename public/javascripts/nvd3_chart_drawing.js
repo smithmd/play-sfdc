f23aa43ce2c6691b23f0d6de4a75aa0fb0e1e7d3
Michael Smith <smithmd@interlochen.org>
2015-06-23 14:33:17 -0400

    more changes

--- public/javascripts/nvd3_chart_drawing.js
+++ public/javascripts/nvd3_chart_drawing.js
@@ -36,7 +36,11 @@ function drawBullet(dashboard, report_id, column) {
     fs.appendChild(legend);
     var div = document.createElement('div');
     div.classList.add('chart');
+    div.classList.add('bullet');
     var svg = document.createElement('svg');
+    div.appendChild(svg);
+    fs.appendChild(div);
+    document.getElementById('col' + column).appendChild(fs);
 
     nv.addGraph(function () {
         var chart = nv.models.bulletChart();
@@ -48,17 +52,4 @@ function drawBullet(dashboard, report_id, column) {
 
         return chart;
     });
-    div.appendChild(svg);
-    fs.appendChild(div);
-    document.getElementById('col' + column).appendChild(fs);
-}
-
-function exampleData() {
-    return {
-        "title": "Revenue",		//Label the bullet chart
-        "subtitle": "US$, in thousands",		//sub-label for bullet chart
-        "ranges": [150, 225, 300],	 //Minimum, mean and maximum values.
-        "measures": [220],		 //Value representing current measurement (the thick blue line in the example)
-        "markers": [250]			 //Place a marker on the chart (the white triangle marker)
-    };
 }
