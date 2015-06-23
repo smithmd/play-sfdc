c563437c31e826fdc23d9b8099aa6765154b9295
Michael Smith <smithmd@interlochen.org>
2015-06-23 15:13:40 -0400

    trying to get numbers to disappear

--- public/javascripts/nvd3_chart_drawing.js
+++ public/javascripts/nvd3_chart_drawing.js
@@ -6,9 +6,9 @@
 function getBulletChartData(value, report_title, range_array, range_label_array) {
     return {
         "title": report_title,
-        "subtitle": "Total, Millions",
+        "subtitle": "Millions, US$",
         "ranges": range_array,
-        "measures": [value / 1000000],
+        "measures": [10],
         "markers": []
     };
 }
