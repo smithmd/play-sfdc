4a90791380f7ea4253a460bcc3e1013e8c98b647
Michael Smith <smithmd@interlochen.org>
2015-06-23 15:05:40 -0400

    trying to get numbers to disappear

--- public/javascripts/nvd3_chart_drawing.js
+++ public/javascripts/nvd3_chart_drawing.js
@@ -62,6 +62,6 @@ function exampleData() {
         "subtitle": "US$, in thousands",		//sub-label for bullet chart
         "ranges": [150, 225, 300],	 //Minimum, mean and maximum values.
         "measures": [220],		 //Value representing current measurement (the thick blue line in the example)
-        "markers": [250]			 //Place a marker on the chart (the white triangle marker)
+        "markers": []			 //Place a marker on the chart (the white triangle marker)
     };
 }
