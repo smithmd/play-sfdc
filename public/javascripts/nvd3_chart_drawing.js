2cee64662bb40abfb23248210ee8c8cecbcefbf6
Michael Smith <smithmd@interlochen.org>
2015-06-23 14:49:31 -0400

    using example data

--- public/javascripts/nvd3_chart_drawing.js
+++ public/javascripts/nvd3_chart_drawing.js
@@ -7,8 +7,8 @@ function getBulletChartData(factMap, report_title, range_array, range_label_arra
     return {
         "title": report_title,
         "subtitle": "Total (Millions)",
-        "ranges": [150,250,300], // range_array,
-        "measures": [260], //[(factMap["T!T"].aggregates[0].value / 1000000).toFixed(2)],
+        "ranges": range_array,
+        "measures": [factMap["T!T"].aggregates[0].value / 1000000],
         "markers": []
     };
 }
@@ -48,10 +48,19 @@ function drawBullet(dashboard, report_id, column) {
         var chart = nv.models.bulletChart();
 
         d3.select(svg)
-            .datum(getBulletChartData(factMap, 'Goal', range_array, range_label_array))
+            .datum(exampleData())
             .transition().duration(1000)
             .call(chart);
 
         return chart;
     });
+}
+function exampleData() {
+    return {
+        "title": "Revenue",		//Label the bullet chart
+        "subtitle": "US$, in thousands",		//sub-label for bullet chart
+        "ranges": [150, 225, 300],	 //Minimum, mean and maximum values.
+        "measures": [220],		 //Value representing current measurement (the thick blue line in the example)
+        "markers": [250]			 //Place a marker on the chart (the white triangle marker)
+    };
 }
