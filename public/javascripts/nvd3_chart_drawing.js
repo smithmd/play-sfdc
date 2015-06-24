ade4e6c09df673afd4e96f72721455458f759b7b
Michael Smith <smithmd@interlochen.org>
2015-06-24 09:14:14 -0400

    removed graph, put markers back

--- public/javascripts/nvd3_chart_drawing.js
+++ public/javascripts/nvd3_chart_drawing.js
@@ -63,6 +63,8 @@ function bulletChartData(measure, report_title, range_array) {
         "subtitle": "US$, in thousands",		//sub-label for bullet chart
         "ranges": range_array,	 //Minimum, mean and maximum values.
         "measures": measure,		 //Value representing current measurement (the thick blue line in the example)
-        "markers": []			 //Place a marker on the chart (the white triangle marker)
+        "markers": [250.0]			 //Place a marker on the chart (the white triangle marker)
     };
 }
+//getBulletChartData(factMap["T!T"].aggregates[0].value,'Goal', range_array,[])
+//getBulletChartData([220.0],'Goal', [150.0, 225.0, 300.0])
