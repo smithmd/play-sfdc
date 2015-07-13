d1c7bb3e37c4e1cd30459710d63a2b8ea47e922c
Michael Smith <smithmd@interlochen.org>
2015-07-13 12:27:47 -0400

    drawing charts based on their metadata

--- public/javascripts/chart_helpers.js
+++ public/javascripts/chart_helpers.js
@@ -4,49 +4,72 @@
 
 // helper functions to get the reports from the huge disgusting json
 function getReportIndex(dashboard, report_id) {
-    var report_index = -1;
+  var report_index = -1;
 
-    dashboard.componentData.forEach(function (element, index) {
-        if (element.componentId == report_id) {
-            report_index = index;
-        }
-    });
+  dashboard.componentData.forEach(function (element, index) {
+    if (element.componentId == report_id) {
+      report_index = index;
+    }
+  });
 
-    return report_index;
+  return report_index;
 }
 
 function getReport(dashboard, report_id) {
-    var report = null;
+  var report = null;
 
-    dashboard.componentData.forEach(function (element, index) {
-        if (element.componentId == report_id) {
-            report = dashboard.componentData[index];
-        }
-    });
+  dashboard.componentData.forEach(function (element, index) {
+    if (element.componentId == report_id) {
+      report = dashboard.componentData[index];
+    }
+  });
 
-    return report;
+  return report;
 }
 
 function getReportByIndex(dashboard, report_index) {
-    return dashboard.componentData[report_index];
+  return dashboard.componentData[report_index];
 }
 
 function getReportTitleByIndex(dashboard, report_index) {
-    return dashboard.dashboardMetadata.components[report_index].header;
+  return dashboard.dashboardMetadata.components[report_index].header;
 }
 
 function getColumnLabels(obj) {
-    var columnLabels = [];
-    for (var columnInfo in obj) {
-        if (obj.hasOwnProperty(columnInfo)) {
-            var index = columnInfo.replace(/FORMULA/, '');
-            columnLabels[index - 1] = obj[columnInfo].label;
-        }
+  var columnLabels = [];
+  for (var columnInfo in obj) {
+    if (obj.hasOwnProperty(columnInfo)) {
+      var index = columnInfo.replace(/FORMULA/, '');
+      columnLabels[index - 1] = obj[columnInfo].label;
     }
+  }
 
-    return columnLabels;
+  return columnLabels;
 }
 
 function getReportMetadataByIndex(dashboard, report_index) {
-    return dashboard.dashboardMetadata.components[report_index];
+  return dashboard.dashboardMetadata.components[report_index];
+}
+
+function getChartType(dashboard, report_index) {
+  return dashboard.dashboardMetadata.components[report_index].properties.visualizationType;
+}
+
+function getChartColumn(dashboard,report_index) {
+  let col = null;
+
+  column:for (let i = 0; i < dashboard.dashboardMetadata.layout.columns.length; i++) {
+    component:for (let j = 0; j < dashboard.dashboardMetadata.layout.columns[i].components.length; j++) {
+      if (report_index == dashboard.dashboardMetadata.layout.columns[i].components[j]) {
+        col = i;
+        break column;
+      }
+    }
+  }
+
+  return col;
+}
+
+function getGroupingName(report) {
+  return report.reportResult.reportMetadata.groupingsDown[0];
 }
