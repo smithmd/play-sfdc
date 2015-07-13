d3e053e5124dc9d0726b242e0b294a9749f4c6fd
Michael Smith <smithmd@interlochen.org>
2015-07-13 16:05:31 -0400

    adding donut to nv page

--- public/javascripts/nvd3_chart_drawing.js
+++ public/javascripts/nvd3_chart_drawing.js
@@ -2,62 +2,108 @@
  * Created by smithmd on 6/23/15.
  */
 
-
-
-
 function drawBullet(dashboard, report_id, column) {
-    // grabbing report to make code easier to read
-    var report_index = getReportIndex(dashboard, report_id);
-    var report = getReportByIndex(dashboard, report_index);
-    var factMap = report.reportResult.factMap;
+  // grabbing report to make code easier to read
+  var report_index = getReportIndex(dashboard, report_id);
+  var report = getReportByIndex(dashboard, report_index);
+  var factMap = report.reportResult.factMap;
 
-    var range_label_array = ['Goal', null, null];
+  var range_label_array = ['Goal', null, null];
 
-    var report_metadata = getReportMetadataByIndex(dashboard, report_index);
-    var range_array = [
-        report_metadata.properties.visualizationProperties.breakPoints[0].breaks[0].upperBound / 1000000,
-        report_metadata.properties.visualizationProperties.breakPoints[0].breaks[1].upperBound / 1000000,
-        report_metadata.properties.visualizationProperties.breakPoints[0].breaks[2].upperBound / 1000000
-    ];
+  var report_metadata = getReportMetadataByIndex(dashboard, report_index);
+  var range_array = [
+    report_metadata.properties.visualizationProperties.breakPoints[0].breaks[0].upperBound / 1000000,
+    report_metadata.properties.visualizationProperties.breakPoints[0].breaks[1].upperBound / 1000000,
+    report_metadata.properties.visualizationProperties.breakPoints[0].breaks[2].upperBound / 1000000
+  ];
 
-    // create and append fieldset
-    var fs = document.createElement('fieldset');
-    // create and append legend
-    var legend = document.createElement('legend');
-    legend.innerHTML = getReportTitleByIndex(dashboard, report_index);
-    fs.appendChild(legend);
-    var div = document.createElement('div');
-    div.classList.add('chart');
-    div.classList.add('bullet');
-    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+  // create and append fieldset
+  var fs = document.createElement('fieldset');
+  // create and append legend
+  var legend = document.createElement('legend');
+  legend.innerHTML = getReportTitleByIndex(dashboard, report_index);
+  fs.appendChild(legend);
+  var div = document.createElement('div');
+  div.classList.add('chart');
+  div.classList.add('bullet');
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
 
-    div.appendChild(svg);
-    fs.appendChild(div);
-    document.getElementById('col' + column).appendChild(fs);
+  div.appendChild(svg);
+  fs.appendChild(div);
+  document.getElementById('col' + column).appendChild(fs);
 
+  var value = [factMap["T!T"].aggregates[0].value / 1000000];
+  var graphData = bulletChartData(value, "Goal", range_array, [null]);
+  nv.addGraph(function () {
+    var chart = nv.models.bulletChart();
 
-    var value = [factMap["T!T"].aggregates[0].value / 1000000];
-    var graphData = bulletChartData(value,"Goal",range_array,[null]);
-    nv.addGraph(function () {
-        var chart = nv.models.bulletChart();
+    d3.select(svg)
+        .datum(graphData)
+        .transition().duration(1000)
+        .call(chart);
 
-        d3.select(svg)
-            .datum(graphData)
-            .transition().duration(1000)
-            .call(chart);
-
-        return chart;
-    });
+    return chart;
+  });
 }
 function bulletChartData(measure, report_title, range_array, markers) {
-    return {
-        "title": report_title,		//Label the bullet chart
-        "subtitle": "Millions of US$",		//sub-label for bullet chart
-        "ranges": range_array,	 //Minimum, mean and maximum values.
-        "measures": measure,		 //Value representing current measurement (the thick blue line in the example)
-        "markers": markers			 //Place a marker on the chart (the white triangle marker)
-    };
+  return {
+    "title": report_title,		//Label the bullet chart
+    "subtitle": "Millions of US$",		//sub-label for bullet chart
+    "ranges": range_array,	 //Minimum, mean and maximum values.
+    "measures": measure,		 //Value representing current measurement (the thick blue line in the example)
+    "markers": markers			 //Place a marker on the chart (the white triangle marker)
+  };
 }
-//getBulletChartData(factMap["T!T"].aggregates[0].value,'Goal', range_array,[])
-//getBulletChartData([220.0],'Goal', [150.0, 225.0, 300.0])
+
+function drawDonut(dashboard, report_id, column) {
+  // grabbing report to make code easier to read
+  var report_index = getReportIndex(dashboard, report_id);
+  var report = getReportByIndex(dashboard, report_index);
+  var factMap = report.reportResult.factMap;
+
+  var data_array = [];
+
+  for (var i = 0; i < report.reportResult.groupingsDown.groupings.length; i++) {
+    data_obj = {
+      "label": report.reportResult.groupingsDown.groupings[i].label,
+      "value": factMap[i + "!T"].aggregates[0]
+    };
+    data_array.push(data_obj);
+  }
+
+  // create and append fieldset
+  var fs = document.createElement('fieldset');
+  // create and append legend
+  var legend = document.createElement('legend');
+  legend.innerHTML = getReportTitleByIndex(dashboard, report_index);
+  fs.appendChild(legend);
+  var div = document.createElement('div');
+  div.classList.add('chart');
+  div.classList.add('bullet');
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+
+  div.appendChild(svg);
+  fs.appendChild(div);
+  document.getElementById('col' + column).appendChild(fs);
+
+  nv.addGraph(function () {
+    var chart = nv.models.pieChart()
+            .x(function (d) {return d.label})
+            .y(function (d) {return d.value})
+            .showLabels(true)     //Display pie labels
+            .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+            .labelType("value") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+            .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
+            .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
+        ;
+
+    d3.select(svg)
+        .datum(data_array)
+        .transition().duration(350)
+        .call(chart);
+
+    return chart;
+  });
+}
