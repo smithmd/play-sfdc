987ccccf1af26aa551570b64880f0779475095f9
Michael Smith <smithmd@interlochen.org>
2015-07-14 09:34:46 -0400

    removed transition from bar chart

--- public/javascripts/nvd3_chart_drawing.js
+++ public/javascripts/nvd3_chart_drawing.js
@@ -155,7 +155,6 @@ function drawColumn(dashboard, report_id, column) {
       .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
       .tooltips(false)        //Don't show tooltips
       .showValues(true)       //...instead, show the bar value right on top of each bar.
-      .transitionDuration(350)
       ;
 
   d3.select(svg)
